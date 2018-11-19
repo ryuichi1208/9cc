9e7a65b583977f2642fdc554d9ada632564be704
Ryuichi1208 <35625782+ryuichi1208@users.noreply.github.com>
2018-11-19 21:56:00 +0900

    Update irdump.c

--- irdump.c
+++ irdump.c
@@ -19,3 +19,73 @@ static char *tostr_call(IR *ir) {
   sb_append(sb, ")");
   return sb_get(sb);
 }
+
+static char *tostr(IR *ir) {
+  int r0 = regno(ir->r0);
+  int r1 = regno(ir->r1);
+  int r2 = regno(ir->r2);
+
+  switch (ir->op) {
+  case IR_ADD:
+    return format("r%d = r%d + r%d", r0, r1, r2);
+  case IR_CALL:
+    return tostr_call(ir);
+  case IR_DIV:
+    return format("r%d = r%d / r%d", r0, r1, r2);
+  case IR_IMM:
+    return format("r%d = %d", r0, ir->imm);
+  case IR_JMP:
+    if (ir->bbarg)
+      return format("JMP .L%d (r%d)", ir->bb1->label, regno(ir->bbarg));
+    return format("JMP .L%d", ir->bb1->label);
+  case IR_LABEL_ADDR:
+    return format("r%d = .L%d", r0, ir->label);
+  case IR_EQ:
+    return format("r%d = r%d == r%d", r0, r1, r2);
+  case IR_NE:
+    return format("r%d = r%d != r%d", r0, r1, r2);
+  case IR_LE:
+    return format("r%d = r%d <= r%d", r0, r1, r2);
+  case IR_LT:
+    return format("r%d = r%d < r%d", r0, r1, r2);
+  case IR_AND:
+    return format("r%d = r%d & r%d", r0, r1, r2);
+  case IR_OR:
+    return format("r%d = r%d | r%d", r0, r1, r2);
+  case IR_XOR:
+    return format("r%d = r%d ^ r%d", r0, r1, r2);
+  case IR_SHL:
+    return format("r%d = r%d << r%d", r0, r1, r2);
+  case IR_SHR:
+    return format("r%d = r%d >> r%d", r0, r1, r2);
+  case IR_LOAD:
+    return format("LOAD%d r%d, r%d", ir->size, r0, r2);
+  case IR_LOAD_SPILL:
+    return format("LOAD_SPILL r%d, %d", r0, ir->imm);
+  case IR_MOD:
+    return format("r%d = r%d %% r%d", r0, r1, r2);
+  case IR_MOV:
+    return format("r%d = r%d", r0, r2);
+  case IR_MUL:
+    return format("r%d = r%d * r%d", r0, r1, r2);
+  case IR_NOP:
+    return "NOP";
+  case IR_RETURN:
+    return format("RET r%d", r2);
+  case IR_STORE:
+    return format("STORE%d r%d, r%d", ir->size, r1, r2);
+  case IR_STORE_ARG:
+    return format("STORE_ARG%d %d %s (%d)", ir->size, ir->imm, ir->var->name,
+                  ir->var->offset);
+  case IR_STORE_SPILL:
+    return format("STORE_SPILL r%d, %d", r1, ir->imm);
+  case IR_SUB:
+    return format("r%d = r%d - r%d", r0, r1, r2);
+  case IR_BPREL:
+    return format("BPREL r%d %s (%d)", r0, ir->var->name, ir->var->offset);
+  case IR_BR:
+    return format("BR r%d .L%d .L%d", r2, ir->bb1->label, ir->bb2->label);
+  default:
+    assert(0 && "unknown op");
+  }
+}

aa3973ee400785b9a06f834d9e15644ffd39786f
Ryuichi1208 <35625782+ryuichi1208@users.noreply.github.com>
2018-11-19 21:55:44 +0900

    Update irdump.c

--- irdump.c
+++ irdump.c
@@ -1,1 +1,21 @@
+#include "9cc.h"
 
+static int regno(Reg *r) {
+  if (!r)
+    return 0;
+  if (r->rn != -1)
+    return r->rn;
+  return r->vn;
+}
+
+static char *tostr_call(IR *ir) {
+  StringBuilder *sb = new_sb();
+  sb_append(sb, format("r%d = %s(", regno(ir->r0), ir->name));
+  for (int i = 0; i < ir->nargs; i++) {
+    if (i != 0)
+      sb_append(sb, ", ");
+    sb_append(sb, format("r%d", regno(ir->args[i])));
+  }
+  sb_append(sb, ")");
+  return sb_get(sb);
+}
